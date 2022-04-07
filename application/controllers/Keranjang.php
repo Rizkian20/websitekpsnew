67513a2eaac0177636d829795278386f62cf56c3
Rizkian20 <rizkianwar020@gmail.com>
2022-04-08 01:20:21 +0700

    update

--- application/controllers/Keranjang.php
+++ application/controllers/Keranjang.php
@@ -5,6 +5,9 @@ class Keranjang extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if (!$this->session->userdata('username')) {
+            redirect('auth');
+        }
         $this->load->library('form_validation');
         $this->load->model('m_keranjang');
         $this->load->library('pagination');
