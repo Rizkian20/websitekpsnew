40509d7d70a520e87f55f82aeb833552ca522224
Rizkian20 <rizkianwar020@gmail.com>
2022-04-08 01:19:51 +0700

    update

--- application/controllers/Katalogadmin.php
+++ application/controllers/Katalogadmin.php
@@ -5,6 +5,9 @@ class Katalogadmin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if (!$this->session->userdata('username')) {
+            redirect('auth');
+        }
         $this->load->library('form_validation');
         $this->load->model('m_katalogadmin');
         $this->load->library('pagination');
